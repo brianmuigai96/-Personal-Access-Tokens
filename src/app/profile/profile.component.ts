1b1db15def6f159d8cf293614d6e57bfbf50cbf4
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-08-23 06:52:52 +0300

    adding public constructors and set and getting repos

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,6 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  profile: any;
+  repos: any[] = [];
+  githubUserQuery: string = 'brianmuigai96';
+
+  constructor(
+    public apiService: ApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+
+  getUser() {
+    this.apiService.getProfileInfo(this.githubUserQuery.split(" ").join("")).subscribe(profile => {
+      this.profile = profile;
+      console.log('profile', this.profile);
+    });
+
+    this.getRepos();
+
+  }
+
+  getRepos(): void {
+    this.apiService.getUserRepos(this.githubUserQuery.split(" ").join("")).subscribe(results => {
+      this.repos = results;
+      console.log('repos', this.repos);
+    });
+
+  }
 
 }
 
