247cae2952ea4fb7b4fbb1ac2f53e01f498049a5
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-08-23 07:04:25 +0300

    getting user profile infor and repo name

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,8 +7,16 @@ export class ApiService {
   username!: string;
   token!: string;
   endPart: any = 'WUlWfnbHTiNHee4YjCuu';
-  
+
   constructor(
     private http: HttpClient
   ) { }
+  
+  getProfileInfo(name: string): Observable<any> {
+    return this.http.get("https://api.github.com/users/" + name + '?access_token=' + 'ghp_IxON6SWJJgzwefwu' + this.endPart);
+  }
+
+  getUserRepos(name: string): Observable<any> {
+    return this.http.get("https://api.github.com/users/" + name + '/repos?access_token=' + 'ghp_IxON6SWJJgzwefwu' + this.endPart);
+  }
 }
